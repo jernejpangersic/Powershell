57605d3add90990f979a41f9164168b093c5f893
Jernej Pangersic (TRENKWALDER <a-jepang@microsoft.com>
2020-09-23 12:25:52 +0200

    updated Fast_User_Creation

--- Fast_User_Creation.ps1
+++ Fast_User_Creation.ps1
@@ -12,11 +12,11 @@ $domain = "@mydomain.com"
 $firstName = Read-Host -Prompt "First name"
 $lastName = Read-Host -Prompt "Last name"
 
-$firstName = RmvSpc(($firstName).Trim()).Replace(" ", "-")
-$lastName = RmvSpc(($lastName).Trim()).Replace(" ", "-")
-$uname = $firstName + "." + $lastName + $domain
-$staff = Read-Host -Prompt "Staff"
-$team = Read-Host -Prompt "Team"
+$firstName = RmvSpc(($firstName).Trim()).Replace(" ", "-") # Removing special characters and replacing space with -
+$lastName = RmvSpc(($lastName).Trim()).Replace(" ", "-") # Removing special characters and replacing space with -
+$uname = $firstName + "." + $lastName + $domain # Constructing username
+$staff = Read-Host -Prompt "Staff" # Is the user staff of student? 1 = Staff, 0 = Student
+$team = Read-Host -Prompt "Team" # Not required, but you can enter the name of the Team, where you want to add the new user
 
 # Based on user input, we define the license for users - Faculty or Student. A1 license will be assigned
 # Important - replace XXXXX with your tenant name (XXXX.onmicrosoft.com)
@@ -37,13 +37,14 @@ Connect-MicrosoftTeams -Credential $credentials
 
 # Parameters for new user
 $params = @{
-    DisplayName = (Get-Culture).TextInfo.ToTitleCase($firstName) + " " + (Get-Culture).TextInfo.ToTitleCase($lastName)
-    UserPrincipalName = $uname
-    UsageLocation = "US"
-    PreferredLanguage  = "en-US"  # Preferred language for users - https://docs.microsoft.com/en-us/previous-versions/commerce-server/ee825488(v=cs.20)?redirectedfrom=MSDN
-    ForceChangePassword = $false
     FirstName = (Get-Culture).TextInfo.ToTitleCase($firstName.ToLower())
     LastName = (Get-Culture).TextInfo.ToTitleCase($lastName.ToLower())
+    DisplayName = (Get-Culture).TextInfo.ToTitleCase($firstName) + " " + (Get-Culture).TextInfo.ToTitleCase($lastName)
+    UserPrincipalName = $uname
+    UsageLocation = "US" # Usage location - fill with two letter code of your country
+    PreferredLanguage  = "en-US"  # Preferred language for users - https://docs.microsoft.com/en-us/previous-versions/commerce-server/ee825488(v=cs.20)?redirectedfrom=MSDN
+    Password = "MyPassword1"    # In this case, the password will be hard-coded. Remove this line if you want to have it generated automatically
+    ForceChangePassword = $false  # Set to true if you want to force the user to change password at first login
 }
 
 # Create new user based on parameters, and add license
